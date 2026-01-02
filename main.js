252a33bc8add871aaad59fbb71c1fa3369bf5385
Mersie <ara1990757123@gmail.com>
2026-01-03 02:54:57 +0500

    home3done

---
+++ main.js
@@ -0,0 +1,56 @@
+// var i = 0;
+
+// console.log(i);
+// console.log(++i);
+// console.log(i++);
+// console.log(i);
+
+
+// var i = 0;
+// while ( i <= 10){
+//     console.log(i);
+//     i++
+
+// }
+
+// 
+// do {
+//     var value = +prompt ("Введиите число")
+// }while (isNaN (value) ) {
+//     console.log(value);
+
+
+// }
+
+// for(var i = 0; i < 10;i++) {
+//     console.log(i);
+
+// }
+// for (let i = 0; i < 20; i++) {
+//     if (i % 2 == 1) {
+//         console.log(i);
+
+//     }
+
+// }
+
+// let even = 0
+// let odd = 0
+// for (let i = 0; i <= 15; i++) {
+//     if (i % 2 == 0) {
+//         even++
+//     } else {
+//         odd++
+//     }
+//     console.log("Количество четных чисел " + even );
+//     console.log("Количество нечетных чисел " + odd );
+
+
+// }
+
+
+let sum = 0
+for (let i = 0; i < 10; i++) {
+    sum *= i
+}
+console.log(sum);
